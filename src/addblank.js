da37c51127262adcd9a40b0481bbdf844dc996de
khaled6mmd <37240519+khaled6mmd@users.noreply.github.com>
2022-08-07 18:44:02 +0200

    enhancement

--- src/addblank.js
+++ src/addblank.js
@@ -1,13 +1,42 @@
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
-import AddBlankEditing from './mediaarialabelediting';
 
 export default class AddBlank extends Plugin {
 
-	static get requires() {
-		return [ AddBlankEditing];
-    }
-    
 	static get pluginName() {
-		return 'addBlank';
+		return 'AddBlank';
+	}
+
+	init() {
+
+		const editor = this.editor;
+
+		editor.model.schema.extend('$text', { allowAttributes: ['fillBlankId', 'contenteditable', 'style'] });
+
+		editor.conversion.for('downcast').attributeToElement({
+			model: 'fillBlankId',
+			view: (attributeValue, writer) => {
+				return writer.createAttributeElement('a', { target: attributeValue, contenteditable: 'true', style: 'background-color:#e5efff;' }, { priority: 5 });
+			},
+			converterPriority: 'high'
+		});
+
+		editor.conversion.for('upcast').attributeToAttribute({
+			view: {
+				name: 'a',
+				key: 'target'
+			},
+			model: 'fillBlankId',
+			converterPriority: 'high'
+		},
+			{
+				view: {
+					name: 'a',
+					key: 'contenteditable'
+				},
+				model: 'fillBlankId',
+				converterPriority: 'high'
+			}
+		);
+
 	}
 }
